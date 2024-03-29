523504c81de6926fcd797a438cb8011b7f51699a
Andrew Biron <andrewbiron@andrews-air.home>
2024-03-29 13:20:30 -0400

    added GET and PUT methods to database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,44 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (id, content) => {
+  console.log('PUT to the database');
+  //create connection to database
+  const jateDb = await openDB('jate', 1);
+
+  //create transaction
+  const tx = jateDb.transaction('jate', 'readwrite');
+
+  //open up the desired object stor
+  const store = tx.objectStore('jate');
+
+  //use the put() method
+  const request = store.put({id: id, content: content});
+
+  const result = await request;
+  console.log('Data saved to the database', result);
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET from the database');
+
+  //create a connection to database including version
+  const jateDb = await openDB('jate', 1);
+
+  //create transaction 
+  const tx = jateDb.transaction('jate', 'readonly');
+
+  //Open up the desired object store
+  const store = tx.objectStore('jate');
+
+  // user the .getALL() method to get all data
+  const request = store.getAll();
+
+  //Get confirmation of the request
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+}
 
 initdb();
