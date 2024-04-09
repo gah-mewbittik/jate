d53bf0d8b2e619305c114a859835a1ac2b86cf8e
Andrew Biron <andrewbiron@andrews-air.home>
2024-04-09 15:10:41 -0400

    removed id param from put method

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,10 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (id, content) => {
+
+
+putDb(2,"hello")
+export const putDb = async (content) => {
   console.log('PUT to the database');
   //create connection to database
   const jateDb = await openDB('jate', 1);
@@ -25,14 +28,14 @@ export const putDb = async (id, content) => {
   const store = tx.objectStore('jate');
 
   //use the put() method
-  const request = store.put({id: 1, value: content});
+  const request = store.put({id: 1, value: content}); //id was 1
 
   const result = await request;
   console.log('Data saved to the database', result);
 }
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => {
+export const getDb = async () => {/////Added id
   console.log('GET from the database');
 
   //create a connection to database including version
@@ -45,7 +48,7 @@ export const getDb = async () => {
   const store = tx.objectStore('jate');
 
   // user the .getALL() method to get all data
-  const request = store.get(1);
+  const request = store.get(1); //was 1
 
   //Get confirmation of the request
   const result = await request;
